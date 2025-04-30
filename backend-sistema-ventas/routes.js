c3bb88c4f863429ab6c17309146e167d219e0ccc
Alejandra <50176363+Alejandrra@users.noreply.github.com>
2025-04-30 09:22:13 -0500

    cambios

--- backend-sistema-ventas/routes.js
+++ backend-sistema-ventas/routes.js
@@ -5,7 +5,6 @@ import express from "express";
 import pool from "./config/db.js";
 const router = express.Router();
 
-import { login_usuario, registrar_usuario , forgotPassword } from './controllers/auth_controller.js';
 
 import usuariosRoutes from './routes/usuarios_routes.js';
 import clientesRoutes from './routes/clientes_routes.js';
@@ -16,6 +15,7 @@ import detalleventasRoutes from './routes/detalle_ventas_routes.js';
 router.post('/login', login_usuario); //llamamos a la ruta login
 router.post('/register', registrar_usuario); //llamamos a la ruta registro
 router.post('/forgot-password', forgotPassword); //ruta de recuperacion de contrasena
+router.post('/reset-password/:resetToken', resetPassword); //ruta para actualizar contraseña
 
 router.use('/usuarios', usuariosRoutes); //llamamos a la ruta
 router.use('/clientes', clientesRoutes); //llamamos a la ruta
